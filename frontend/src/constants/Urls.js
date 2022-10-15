06a48c77b6523b3bbccbe086e5855bf5579e7eea
SandeepKushajVithanage <sandeepkushaj3@gmail.com>
2022-10-15 17:21:36 +0530

    deploy

--- frontend/src/constants/Urls.js
+++ frontend/src/constants/Urls.js
@@ -1,10 +1,9 @@
-const PRODUCTION_BASE_URL =
-  "https://divisional-secretariat-backend.herokuapp.com/";
+const PRODUCTION_BASE_URL = "https://divisional-secretariat.herokuapp.com/";
 const DEVELOPMENT_BASE_URL = "http://192.168.1.10:8000/";
 
 const Urls = {
-  BASE_URI: DEVELOPMENT_BASE_URL,
-  FILE_STORAGE: DEVELOPMENT_BASE_URL,
+  BASE_URI: PRODUCTION_BASE_URL,
+  FILE_STORAGE: PRODUCTION_BASE_URL,
   CURRENT_USER: "api/me",
   NEWS: "api/news",
   AREA: "api/area",
